0c77a6fa2c39e51a53a5f6334c890dff4d841c70
UnawareITry <krishnendu.marathe@gmail.com>
2026-08-29 01:10:22 +0530

    testing - infinity loop

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'ce25123b24f909055c193132116822667599bf95'
+AESD_ASSIGNMENTS_VERSION = '79a3887c9925129e70954837dcba29da17cdb678'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -36,4 +36,7 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
 
+AESD_ASSIGNMENTS_MODULE_SUBDIRS = aesd-char-driver
+
+$(eval $(kernel-module))
 $(eval $(generic-package))
